c7faefe1002e242bb2f1def2df845507d512f187
Cortney Robinson <gvestudios@gmail.com>
2014-03-26 19:22:26 -0400

    updated readme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
 
+	var seeCode = $('.seeCode'),
+	preCode = $('.coder'),
+	preCodeParent = preCode.parent();
 
-  //google webfont inclusion
-  WebFontConfig = {
-    google: { families: ['Abril+Fatface::latin','Vollkorn:400,400italic:latin','Open+Sans:400,300,400italic,300italic:latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })(); 
+	preCode.hide();
+	(function(){		
+		seeCode.on('click',function(){
+			var current = $(this);
+			current.next(preCode).toggle('slow');
+			
+		});
+	}());
+
 
 
 });
